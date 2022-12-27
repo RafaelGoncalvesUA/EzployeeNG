b59e8f23d3da448e99da39735dbb6a93dcb4a16d
RafaelGoncalvesUA <rfg@ua.pt>
2022-12-27 19:40:33 +0000

    move files to root directory

--- server/app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from app.models import *
-
-# Register your models here.
-admin.register(Company)
-admin.register(Offer)
-admin.register(User)
-admin.register(Comment)
