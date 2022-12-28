26f559c601b1c9a4260edfbf191751f364b99d27
Leonardo Almeida <leonardoalmeida7@ua.pt>
2022-12-28 16:04:18 +0000

    remove django webpage

--- backend/Ezployee/urls.py
+++ backend/Ezployee/urls.py
@@ -11,25 +11,6 @@ from app.views.webservice import ws_auth, ws_offers, ws_companies, ws_comments, 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # website
-    path("", index.index, name="index"),
-    path("register/", auth.register, name="register"),
-    path("register_company/", auth.register_company, name="register_company"),
-    path("login/", auth.login, name="login"),
-    path("logout/", auth.logout, name="login"),
-    path("reserved/", reserved.reserved, name="reserved_area"),
-    path("reserved/edit", reserved.edit, name="edit"),
-    path("reserved/offer", reserved.add_offer, name="add_offer"),
-    path("reserved/offers", reserved.company_offers, name="offers"),
-    path("reserved/offers/<int:offer_id>", reserved.edit_offer, name="edit_offer"),
-    path("reserved/comments", reserved.company_comments, name="comments"),
-    path("companies/<str:name>/<int:rating>/<int:order>/", companies.companies, name="companies"),
-    path("company/<int:id>/", company.company, name="company"),
-    path("favorite/<str:action>/<str:type>/<int:id>/", favorite.favorite, name="favorite"),
-    path("myfavorites/", myfavorites.myfavorites, name="myfavorites"),
-    path("offers/<int:offer_id>", offers.offer_detail, name="offer_detail"),
-    path("offers/", offers.offers, name="offers"),
-
     # api
     path("api/login/", ws_auth.login),
     path("api/user/register/", ws_auth.register_user),
