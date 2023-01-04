9ea06b5ddd0af684529bf10fffbb453c77abe139
RafaelGoncalvesUA <rfg@ua.pt>
2023-01-04 12:24:53 +0000

    Auto-generated commit [ci skip]

--- backend/app/security.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.authentication import TokenAuthentication
-from rest_framework.decorators import authentication_classes, permission_classes
-from rest_framework.permissions import IsAuthenticated, BasePermission
-
-class IsGetRequest(BasePermission):
-    def has_permission(self, request, view):
-        return request.method == 'GET'
--- src/app/classes/Reply.ts
+++
@@ -1,7 +0,0 @@
-export class Reply {
-    id: number;
-    text: string;
-    name: number;
-    img_url: string;
-    time: string;
-}
